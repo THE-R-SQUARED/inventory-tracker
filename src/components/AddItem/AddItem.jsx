79460e49cb209ed9c9ef7c0d1eb2cd04ad87d7f4
Richmond Rimando <richmond486@gmail.com>
2021-12-11 23:40:38 -0800

    resolve merge conflicts

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Button, Modal, Form } from 'semantic-ui-react'
-
-import WIDTHS from '../../constants/form-styling'
+import { SMALL, MEDIUM, LARGE, XLARGE, ABSOLUTE } from '../../constants/form-styling'
 
 const AddItem = () => {
   const [open, isModalOpen] = React.useState(false)
@@ -21,19 +20,19 @@ const AddItem = () => {
               required
               label="Name"
               placeholder="Air Jordan 1 High Mocha"
-              width={WIDTHS.XLARGE}
+              width={XLARGE}
             />
-            <Form.Input required label="Size" placeholder="10" width={WIDTHS.SMALL} />
-            <Form.Input label="Color" placeholder="Brown" width={WIDTHS.MEDIUM} />
-            <Form.Input label="SKU" placeholder="555088-105" width={WIDTHS.LARGE} />
+            <Form.Input required label="Size" placeholder="10" width={SMALL} />
+            <Form.Input label="Color" placeholder="Brown" width={MEDIUM} />
+            <Form.Input label="SKU" placeholder="555088-105" width={LARGE} />
           </Form.Group>
           <Form.Group>
-            <Form.Input label="Purchased Where" placeholder="Footlocker" width={WIDTHS.LARGE} />
-            <Form.Input label="Purchase Date" placeholder="08/25/2022" width={WIDTHS.MEDIUM} />
-            <Form.Input label="Retail Price" placeholder="$180" width={WIDTHS.SMALL} />
-            <Form.Input label="Tax" placeholder="$3.50" width={WIDTHS.SMALL} />
-            <Form.Input label="Shipping" placeholder="$10" width={WIDTHS.SMALL} />
-            <Form.Input required label="Purchase Total" placeholder="$204" width={WIDTHS.SMALL} />
+            <Form.Input label="Purchased Where" placeholder="Footlocker" width={LARGE} />
+            <Form.Input label="Purchase Date" placeholder="08/25/2022" width={MEDIUM} />
+            <Form.Input label="Retail Price" placeholder="$180" width={SMALL} />
+            <Form.Input label="Tax" placeholder="$3.50" width={SMALL} />
+            <Form.Input label="Shipping" placeholder="$10" width={SMALL} />
+            <Form.Input required label="Purchase Total" placeholder="$204" width={SMALL} />
           </Form.Group>
           <Form.Group>
             <Form.Input
@@ -41,7 +40,7 @@ const AddItem = () => {
               placeholder="Sold to John Smith at Kobey's Swapmeet"
               control="textarea"
               rows="3"
-              width={WIDTHS.ABSOLUTE}
+              width={ABSOLUTE}
             />
           </Form.Group>
         </Form>
