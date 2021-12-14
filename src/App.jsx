c3451e84bc93a8a8d91d92bbbf8bc45c21a47efd
RudySj <sernar1@uci.edu>
2021-12-14 12:09:45 -0800

    feat: add initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,123 @@
 import React from 'react'
+import { Grid, Menu, Sidebar, Icon, Container } from 'semantic-ui-react'
+// import styled from 'styled-components'
 
-import MenuBar from './layouts/MenuBar'
-import SideBar from './layouts/SideBar'
-import InventoryPage from './pages/InventoryPage/InventoryPage'
+import './styles/app.css'
+
+// import MenuBar from './layouts/MenuBar'
+// import SideBar from './layouts/SideBar'
+// import InventoryPage from './pages/InventoryPage/InventoryPage'
+
+// const GridContainer = styled(Grid)({
+//   minHeight: '100vh',
+// })
 
 const App = () => (
-  <SideBar>
-    <MenuBar />
-    <InventoryPage />
-  </SideBar>
+  <Grid stretched>
+    <Grid.Row>
+      <Grid.Column>
+        <Sidebar
+          as={Menu}
+          icon="labeled"
+          inverted
+          vertical
+          visible
+          width="thin"
+          direction="left"
+          left
+        >
+          <Menu.Item as="a">
+            <Icon name="home" />
+            Home
+          </Menu.Item>
+          <Menu.Item as="a">
+            <Icon name="gamepad" />
+            Games
+          </Menu.Item>
+          <Menu.Item as="a">
+            <Icon name="camera" />
+            Channels
+          </Menu.Item>
+        </Sidebar>
+        <Sidebar.Pusher>
+          <Container fluid>
+            <Grid stackable>
+              <Grid.Row>
+                <Grid.Column>
+                  <Container> test1</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test2</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test3</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test4</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test5</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test6</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test7</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test8</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test9</Container>
+                </Grid.Column>
+                <Grid.Column>
+                  <Container> test10</Container>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Sidebar.Pusher>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 )
+
+/*
+  <div>
+    <div className="ui grid">
+      <div className="row">
+        <div className="column">
+          <div id="maincontent" className="ui bottom attached segment pushable">
+            <div id="sidebar" className="ui visible left vertical sidebar menu">
+              first
+            </div>
+            <div id="content" className="pusher">
+              <div className="ui basic segment">
+                <h3 className="ui header">Application Content</h3>
+                <p>First paragraph...</p>
+                <p>Second paragraph...</p>
+                <p>Third paragraph...</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+*/
+
+/*
+
+  <GridContainer>
+    <Grid columns={2} style={{ minHeight: '100vh' }}>
+      <Grid.Column width={3}>
+        <SideBar />
+      </Grid.Column>
+      <Grid.Column>
+        <Button />
+      </Grid.Column>
+    </Grid>
+  </GridContainer>
+  */
 
 export default App
--- src/layouts/SideBar/SideBar.jsx
+++ src/layouts/SideBar/SideBar.jsx
@@ -1,36 +1,51 @@
 import React from 'react'
-import { Sidebar, Menu, Icon, Segment } from 'semantic-ui-react'
+import { Menu, Container } from 'semantic-ui-react'
 
-const SideBar = ({ children }) => (
-  <Sidebar.Pushable
-    as={Segment}
-    style={{ border: 0, minHeight: '100vh', borderRadius: 0 }}
-  >
-    <Sidebar
-      as={Menu}
-      animation="push"
-      icon="labeled"
-      inverted
-      vertical
-      visible
-      width="thin"
-    >
-      <Menu.Item as="a">
-        <Icon name="home" />
-        Home
-      </Menu.Item>
-      <Menu.Item as="a">
-        <Icon name="book" />
-        Inventory
-      </Menu.Item>
-      <Menu.Item as="a">
-        <Icon name="dollar sign" />
-        Sales
-      </Menu.Item>
-    </Sidebar>
+const SideBar = () => {
+  // const [activeItem, setActiveItem] = useState(null)
 
-    <Sidebar.Pusher>{children}</Sidebar.Pusher>
-  </Sidebar.Pushable>
-)
+  // const handleItemClick = () => setActiveItem('clicked')
+
+  return (
+    <Menu pointing secondary>
+      <Container>
+        <Menu.Item name="Landing" />
+        <Menu.Item name="home" />
+        <Menu.Item name="Account" />
+      </Container>
+    </Menu>
+  )
+}
 
 export default SideBar
+
+/*
+    <Menu icon="labeled" vertical borderless size="small">
+      <Menu.Item
+        name="gamepad"
+        active={activeItem === 'gamepad'}
+        onClick={handleItemClick}
+      >
+        <Icon name="gamepad" />
+        Games
+      </Menu.Item>
+
+      <Menu.Item
+        name="video camera"
+        active={activeItem === 'video camera'}
+        onClick={handleItemClick}
+      >
+        <Icon name="video camera" />
+        Channels
+      </Menu.Item>
+
+      <Menu.Item
+        name="video play"
+        active={activeItem === 'video play'}
+        onClick={handleItemClick}
+      >
+        <Icon name="video play" />
+        Videos
+      </Menu.Item>
+    </Menu>
+*/
