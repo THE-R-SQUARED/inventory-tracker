b39f6954f3b9dc91d271793c4ab5a9abc1cc65c8
RudySj <sernar1@uci.edu>
2021-12-14 22:50:36 -0800

    feat: move sidebar into layout

--- src/layouts/MenuBar/index.jsx
+++ src/layouts/MenuBar/index.jsx
@@ -1,3 +1,19 @@
-import MenuBar from './MenuBar'
+import React from 'react'
+import { Grid, Header } from 'semantic-ui-react'
+import UserProfile from '../../components/UserProfile'
+
+const MenuBar = () => (
+  // <Container fluid>
+  <Grid columns={2} stackable>
+    <Grid.Row>
+      <Grid.Column textAlign="left">
+        <Header>Dashboard</Header>
+      </Grid.Column>
+      <Grid.Column textAlign="right">
+        <UserProfile />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
 
 export default MenuBar
--- src/layouts/SideBar/index.jsx
+++ src/layouts/SideBar/index.jsx
@@ -1,3 +1,16 @@
-import SideBar from './SideBar'
+import React from 'react'
+import styled from 'styled-components'
+
+import SideBarMenu from '../../components/SideBarMenu'
+
+const SideBarLayout = styled.div({
+  width: '170px',
+})
+
+const SideBar = () => (
+  <SideBarLayout>
+    <SideBarMenu />
+  </SideBarLayout>
+)
 
 export default SideBar
