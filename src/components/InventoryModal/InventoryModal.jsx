f094d1fcad0f9066c585a98e0480c42a89a5146e
Richmond Rimando <richmond486@gmail.com>
2021-12-16 00:12:04 -0800

    Feature modal refactor (#12)
    * feat: refactor modal components
    
    * feat: cleaned some code
    
    * feat: refactor inventory modals more

---
+++ src/components/InventoryModal/InventoryModal.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Button, Modal, ModalContent } from 'semantic-ui-react'
+// import AddItem from '../AddItem'
+// import SoldItem from '../SoldItem'
+// import ModifyItem from '../ModifyItem'
+// import RemoveItem from '../RemoveItem'
+import '../../styles/modal.css'
+
+const InventoryModal = ({
+  children,
+  modalButtonText,
+  modalHeaderText,
+  modalColor,
+  modalIcon,
+  modalActionButtonText,
+  modalCSS,
+}) => {
+  const [open, isModalOpen] = React.useState(false)
+
+  return (
+    <Modal
+      onClose={() => isModalOpen(false)}
+      onOpen={() => isModalOpen(true)}
+      open={open}
+      trigger={
+        <Button inverted color={modalColor}>
+          {modalButtonText}
+        </Button>
+      }
+    >
+      <Modal.Header id={modalCSS}>{modalHeaderText}</Modal.Header>
+
+      <ModalContent>{children}</ModalContent>
+
+      <Modal.Actions>
+        <Button color="black" onClick={() => isModalOpen(false)}>
+          Cancel
+        </Button>
+        <Button
+          content={modalActionButtonText}
+          labelPosition="right"
+          icon={modalIcon}
+          inverted
+          onClick={() => isModalOpen(false)}
+          color={modalColor}
+        />
+      </Modal.Actions>
+    </Modal>
+  )
+}
+
+export default InventoryModal
--- src/components/ModifyItem/ModifyItem.jsx
+++ src/components/ModifyItem/ModifyItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Modal, Form } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
+import InventoryModal from '../InventoryModal'
 import {
   SMALL,
   MEDIUM,
@@ -7,110 +8,89 @@ import {
   XLARGE,
   ABSOLUTE,
 } from '../../constants/form-styling'
-import '../../styles/modal.css'
 
 const ModifyItem = () => {
-  const [open, isModalOpen] = React.useState(false)
-
   return (
-    <Modal
-      onClose={() => isModalOpen(false)}
-      onOpen={() => isModalOpen(true)}
-      open={open}
-      trigger={
-        <Button inverted color="yellow">
-          Edit
-        </Button>
-      }
+    <InventoryModal
+      modalButtonText="Edit"
+      modalHeaderText="Edit Item"
+      modalColor="yellow"
+      modalIcon="edit"
+      modalActionButtonText="Edit"
+      modalCSS="modify-item-modal-header"
     >
-      <Modal.Header id="modify-item-modal-header">Edit Item</Modal.Header>
-      <Modal.Content>
-        <Form>
-          <Form.Group>
-            <Form.Input
-              required
-              label="Name"
-              placeholder="Air Jordan 1 High Mocha"
-              width={XLARGE}
-            />
-            <Form.Input required label="Size" placeholder="10" width={SMALL} />
-            <Form.Input label="Color" placeholder="Brown" width={MEDIUM} />
-            <Form.Input label="SKU" placeholder="555088-105" width={LARGE} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Input
-              label="Purchased Where"
-              placeholder="Footlocker"
-              width={LARGE}
-            />
-            <Form.Input
-              label="Purchase Date"
-              placeholder="08/25/2022"
-              width={MEDIUM}
-            />
-            <Form.Input label="Retail Price" placeholder="$180" width={SMALL} />
-            <Form.Input label="Tax" placeholder="$3.50" width={SMALL} />
-            <Form.Input label="Shipping" placeholder="$10" width={SMALL} />
-            <Form.Input
-              required
-              label="Purchase Total"
-              placeholder="$204"
-              width={SMALL}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Input
-              required
-              label="Sold Where"
-              placeholder="StockX"
-              width={LARGE}
-            />
-            <Form.Input
-              required
-              label="Sell Date"
-              placeholder="08/26/2022"
-              width={MEDIUM}
-            />
-            <Form.Input
-              required
-              label="Sell Price"
-              placeholder="$1000"
-              width={SMALL}
-            />
-            <Form.Input label="Tax" placeholder="$3.50" width={SMALL} />
-            <Form.Input label="Shipping" placeholder="$10" width={SMALL} />
-            <Form.Input
-              required
-              label="Sell Total"
-              placeholder="$950"
-              width={SMALL}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Input
-              label="Notes"
-              placeholder="Sold to John Smith at Kobey's Swapmeet"
-              control="textarea"
-              rows="3"
-              width={ABSOLUTE}
-            />
-          </Form.Group>
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color="black" onClick={() => isModalOpen(false)}>
-          Cancel
-        </Button>
-        <Button
-          content="Edit"
-          labelPosition="right"
-          icon="edit"
-          inverted
-          onClick={() => isModalOpen(false)}
-          color="yellow"
-        />
-      </Modal.Actions>
-    </Modal>
+      <Form>
+        <Form.Group>
+          <Form.Input
+            required
+            label="Name"
+            placeholder="Air Jordan 1 High Mocha"
+            width={XLARGE}
+          />
+          <Form.Input required label="Size" placeholder="10" width={SMALL} />
+          <Form.Input label="Color" placeholder="Brown" width={MEDIUM} />
+          <Form.Input label="SKU" placeholder="555088-105" width={LARGE} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Input
+            label="Purchased Where"
+            placeholder="Footlocker"
+            width={LARGE}
+          />
+          <Form.Input
+            label="Purchase Date"
+            placeholder="08/25/2022"
+            width={MEDIUM}
+          />
+          <Form.Input label="Retail Price" placeholder="$180" width={SMALL} />
+          <Form.Input label="Tax" placeholder="$3.50" width={SMALL} />
+          <Form.Input label="Shipping" placeholder="$10" width={SMALL} />
+          <Form.Input
+            required
+            label="Purchase Total"
+            placeholder="$204"
+            width={SMALL}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Input
+            required
+            label="Sold Where"
+            placeholder="StockX"
+            width={LARGE}
+          />
+          <Form.Input
+            required
+            label="Sell Date"
+            placeholder="08/26/2022"
+            width={MEDIUM}
+          />
+          <Form.Input
+            required
+            label="Sell Price"
+            placeholder="$1000"
+            width={SMALL}
+          />
+          <Form.Input label="Tax" placeholder="$3.50" width={SMALL} />
+          <Form.Input label="Shipping" placeholder="$10" width={SMALL} />
+          <Form.Input
+            required
+            label="Sell Total"
+            placeholder="$950"
+            width={SMALL}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Input
+            label="Notes"
+            placeholder="Sold to John Smith at Kobey's Swapmeet"
+            control="textarea"
+            rows="3"
+            width={ABSOLUTE}
+          />
+        </Form.Group>
+      </Form>
+    </InventoryModal>
   )
 }
 
