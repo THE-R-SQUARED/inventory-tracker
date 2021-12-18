66a87ac0c606f6c6ce49a88dc93839575732b2cb
Richmond Rimando <richmond486@gmail.com>
2021-12-17 21:23:55 -0800

    Feature add item modal (#17)
    * feat: add disabled form fields and checkbox
    
    * feat: add toggleable disabled form fields by means of a checkbox

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React, { useState } from 'react'
+
+import { Form, Checkbox } from 'semantic-ui-react'
 import InventoryModal from '../InventoryModal'
 import {
   SMALL,
@@ -10,6 +11,36 @@ import {
 } from '../../constants/form-styling'
 
 const AddItem = () => {
+  const [disabled, toggleDisabledOnSoldFields] = useState(true)
+
+  const [formFields, setFormFields] = useState({})
+
+  const {
+    soldWhere = '',
+    soldDate = '',
+    soldPrice = '',
+    soldTax = '',
+    soldShipping = '',
+    soldTotal = '',
+  } = formFields
+
+  // // name, value
+  const handleChange = (e, { name, value }) => {
+    // console.log({ name, value })
+    setFormFields((prevFormFields) => ({
+      ...prevFormFields,
+      [name]: value,
+    }))
+  }
+
+  // console.log(soldWhere)
+  // useEffect(() => {
+  //   setFormFields((prevFormFields) => ({
+  //     ...prevFormFields,
+  //     soldDate: soldWhere,
+  //   }))
+  // }, [soldWhere])
+
   return (
     <InventoryModal
       modalButtonText="Add Item"
@@ -23,7 +54,7 @@ const AddItem = () => {
         <Form.Group>
           <Form.Input
             required
-            label="Name"
+            label="vendor"
             placeholder="Air Jordan 1 High Mocha"
             width={XLARGE}
           />
@@ -50,6 +81,78 @@ const AddItem = () => {
             label="Purchase Total"
             placeholder="$204"
             width={SMALL}
+          />
+        </Form.Group>
+        <br />
+        <div>
+          <Checkbox
+            label="Item Already Sold?"
+            onChange={() => {
+              setFormFields({})
+              toggleDisabledOnSoldFields(!disabled)
+            }}
+          />
+        </div>
+        <br />
+        <Form.Group>
+          {/* <Message header={JSON.stringify(formFields)} /> */}
+          <Form.Input
+            required
+            label="Sold Where"
+            placeholder="StockX"
+            width={LARGE}
+            disabled={disabled}
+            name="soldWhere"
+            value={soldWhere}
+            onChange={handleChange}
+          />
+          <Form.Input
+            required
+            label="Sell Date"
+            placeholder="08/26/2022"
+            width={MEDIUM}
+            disabled={disabled}
+            name="soldDate"
+            value={soldDate}
+            onChange={handleChange}
+          />
+          <Form.Input
+            required
+            label="Sell Price"
+            placeholder="$1000"
+            width={SMALL}
+            disabled={disabled}
+            name="soldPrice"
+            value={soldPrice}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="Tax"
+            placeholder="$3.50"
+            width={SMALL}
+            disabled={disabled}
+            name="soldTax"
+            value={soldTax}
+            onChange={handleChange}
+          />
+          <Form.Input
+            label="Shipping"
+            placeholder="$10"
+            width={SMALL}
+            disabled={disabled}
+            name="soldShipping"
+            value={soldShipping}
+            onChange={handleChange}
+          />
+          <Form.Input
+            required
+            label="Sell Total"
+            placeholder="$950"
+            width={SMALL}
+            disabled={disabled}
+            name="soldTotal"
+            value={soldTotal}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group>
