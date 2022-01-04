93d12949474b522b7e3cfa68835bdcf700e4d6b1
Richmond Rimando <richmond486@gmail.com>
2022-01-03 23:59:07 -0800

    feat: remove columns widths

--- src/components/InventoryTable/InventoryTable.jsx
+++ src/components/InventoryTable/InventoryTable.jsx
@@ -6,43 +6,51 @@ const InventoryTable = () => {
     <MaterialTable
       title="Inventory"
       columns={[
-        { title: 'Name', field: 'itemName', width: 200 },
-        { title: 'Size', field: 'itemSize', width: 200 },
-        { title: 'Color', field: 'itemColor', width: 200 },
-        { title: 'SKU', field: 'itemSKU', width: 200 },
-        { title: 'Purchased Where', field: 'itemPurchaseWhere', width: 200 },
-        { title: 'Purchase Date', field: 'itemPurchaseDate', width: 200 },
+        { title: 'Name', field: 'itemName' },
+        { title: 'Size', field: 'itemSize' },
+        { title: 'Color', field: 'itemColor' },
+        { title: 'SKU', field: 'itemSKU' },
+        { title: 'Purchased Where', field: 'itemPurchaseWhere' },
+        { title: 'Purchase Date', field: 'itemPurchaseDate' },
         {
           title: 'Retail Price',
           field: 'itemPurchaseRetail',
           type: 'numeric',
-          width: 200,
         },
         {
           title: 'Tax (Purchase)',
           field: 'itemPurchaseTax',
           type: 'numeric',
-          width: 200,
         },
         {
           title: 'Shipping (Purchase)',
           field: 'itemPurchaseShipping',
           type: 'numeric',
-          width: 200,
         },
         {
           title: 'Purchase Total',
           field: 'itemPurchaseTotal',
           type: 'numeric',
-          width: 200,
         },
-        { title: 'Sold Where', field: 'soldWhere', width: 200 },
-        { title: 'Sell Date', field: 'soldDate', width: 200 },
-        { title: 'Sell Price', field: 'soldPrice', width: 200 },
-        { title: 'Tax (Sell)', field: 'soldTax', width: 200 },
-        { title: 'Shipping (Sell)', field: 'soldShipping', width: 200 },
-        { title: 'Sell Total', field: 'soldTotal', width: 200 },
-        { title: 'Notes', field: 'notes', width: 200 },
+        { title: 'Sold Where', field: 'soldWhere' },
+        { title: 'Sell Date', field: 'soldDate' },
+        {
+          title: 'Sell Price',
+          field: 'soldPrice',
+          type: 'numeric',
+        },
+        { title: 'Tax (Sell)', field: 'soldTax', type: 'numeric' },
+        {
+          title: 'Shipping (Sell)',
+          field: 'soldShipping',
+          type: 'numeric',
+        },
+        {
+          title: 'Sell Total',
+          field: 'soldTotal',
+          type: 'numeric',
+        },
+        { title: 'Notes', field: 'notes' },
       ]}
       data={[{ itemName: 'Air Jordan 1 Mocha' }]}
       options={{
@@ -50,7 +58,6 @@ const InventoryTable = () => {
         fixedColumns: {
           left: 1,
         },
-        // tableLayout: 'fixed',
       }}
     />
   )
