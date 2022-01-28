dcd319808ba57f0a4731f77efbfa358941ba3544
Richmond Rimando <richmond486@gmail.com>
2022-01-28 02:10:20 -0800

    feat: add item model

--- backend/models/item.model.js
+++ backend/models/item.model.js
@@ -1,1 +1,29 @@
-//Item Model
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    itemName: { type: String, required: true },
+    itemSize: { type: String, required: false },
+    itemColor: { type: String, required: false },
+    itemSKU: { type: String, required: false },
+    itemPurchaseWhere: { type: String, required: false },
+    itemPurchaseDate: { type: String, required: false },
+    itemPurchaseRetail: { type: String, required: false },
+    itemPurchaseTax: { type: String, required: false },
+    itemPurchaseShipping: { type: String, required: false },
+    itemPurchaseTotal: { type: String, required: true },
+    soldWhere: { type: String, required: false },
+    soldDate: { type: String, required: false },
+    soldPrice: { type: String, required: false },
+    soldTax: { type: String, required: false },
+    soldShipping: { type: String, required: false },
+    soldTotal: { type: String, required: false },
+    notes: { type: String, required: false },
+}, {
+    timestamps: true,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
