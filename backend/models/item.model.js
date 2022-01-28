3ceb531d43e15ca666a6e586f3077d80f62b415b
Richmond Rimando <richmond486@gmail.com>
2022-01-28 03:12:44 -0800

    feat: edit item schema

--- backend/models/item.model.js
+++ backend/models/item.model.js
@@ -2,23 +2,26 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+// for mongoose schema type double:
+// https://www.npmjs.com/package/@mongoosejs/double
+
 const itemSchema = new Schema({
     itemName: { type: String, required: true },
     itemSize: { type: String, required: false },
     itemColor: { type: String, required: false },
     itemSKU: { type: String, required: false },
     itemPurchaseWhere: { type: String, required: false },
-    itemPurchaseDate: { type: String, required: false },
-    itemPurchaseRetail: { type: String, required: false },
-    itemPurchaseTax: { type: String, required: false },
-    itemPurchaseShipping: { type: String, required: false },
-    itemPurchaseTotal: { type: String, required: true },
+    itemPurchaseDate: { type: Date, required: false },
+    itemPurchaseRetail: { type: Number, required: false },
+    itemPurchaseTax: { type: Number, required: false },
+    itemPurchaseShipping: { type: Number, required: false },
+    itemPurchaseTotal: { type: Number, required: true },
     soldWhere: { type: String, required: false },
-    soldDate: { type: String, required: false },
-    soldPrice: { type: String, required: false },
-    soldTax: { type: String, required: false },
-    soldShipping: { type: String, required: false },
-    soldTotal: { type: String, required: false },
+    soldDate: { type: Date, required: false },
+    soldPrice: { type: Number, required: false },
+    soldTax: { type: Number, required: false },
+    soldShipping: { type: Number, required: false },
+    soldTotal: { type: Number, required: false },
     notes: { type: String, required: false },
 }, {
     timestamps: true,
