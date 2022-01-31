cc22039c6dbad0ac51b35180720c8137c8e331f3
Richmond Rimando <richmond486@gmail.com>
2022-01-31 00:29:26 -0800

    bug: fix delete item endpoint bug

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -57,10 +57,10 @@ router.route('/:id').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').get((req, res) => {
+router.route('/:id').delete((req, res) => {
     Item.findByIdAndDelete(req.params.id)
         .then(() => res.json('Item deleted.'))
-        .catch.catch(err => res.status(400).json('Error: ' + err));
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/update/:id').post((req, res) => {
